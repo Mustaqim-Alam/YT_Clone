d5a2c2639551195c31685a25ccf81141676fe395
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-10-14 00:39:33 +0530

    YT Clone

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,7 +1,1 @@
-const GOOGLE_API_KEY = "AIzaSyCeWTeKthvTmZmQppDSZIREkeFzOWERSng";
-
-export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+
-  GOOGLE_API_KEY;
-
 
